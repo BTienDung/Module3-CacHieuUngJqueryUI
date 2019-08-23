832eda9cf567139789e80bf16114276f04f78b10
BTienDung <tiendung0899@gmail.com>
2019-08-23 09:18:01 +0700

    'nb'

---
+++ Js/scrip.js
@@ -0,0 +1,23 @@
+$(function () {
+    $("#draggable").draggable();
+});
+
+$(function () {
+    $("#draggable1").draggable();
+    $( "#droppable" ).droppable({
+        drop: function( event, ui ) {
+            $( this )
+                .addClass( "ui-state-highlight" )
+                .find( "p" )
+                .html( "Dropped!" );
+        }
+    });
+});
+
+$(function () {
+   $("#resizable").resizable();
+});
+
+$( function() {
+    $( "#selectable" ).selectable();
+} );
